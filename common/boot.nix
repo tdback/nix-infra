bcb0c0837f6118f35698265432888346bcc32db5
tdback <tyler@tdback.net>
2026-07-12 10:15:03 -0400

    limit generations

--- common/boot.nix
+++ common/boot.nix
@@ -3,7 +3,14 @@
   lib,
   ...
 }:
+let
+  maxGenerations = 8;
+in
 {
   boot.tmp.useTmpfs = lib.mkDefault true;
   boot.tmp.cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
+
+  # limit generations to avoid filling up "/boot"
+  boot.loader.grub.configurationLimit = lib.mkDefault maxGenerations;
+  boot.loader.systemd-boot.configurationLimit = lib.mkDefault maxGenerations;
 }
