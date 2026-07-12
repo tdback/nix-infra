8fb96ce9346c231305d07b7e16ddb6f3dee9f823
tdback <tyler@tdback.net>
2026-07-12 14:48:29 +0000

    default firewall rules

--- common/network.nix
+++ common/network.nix
@@ -1,3 +1,12 @@
 {
+  lib,
+  desktop,
+  ...
+}:
+{
   networking.domain = "tdback.net";
+
+  # use nftables in place of iptables
+  networking.nftables.enable = true;
+  networking.firewall.allowPing = lib.mkDefault (!desktop);
 }
