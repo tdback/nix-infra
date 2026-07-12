7072c0fea3d2d52672fb7ffc9956bffd6cc4f50e
tdback <tyler@tdback.net>
2026-07-12 14:50:52 +0000

    use systemd-resolved for DoT

--- common/network.nix
+++ common/network.nix
@@ -9,4 +9,18 @@
   # use nftables in place of iptables
   networking.nftables.enable = true;
   networking.firewall.allowPing = lib.mkDefault (!desktop);
+
+  # upstream servers to be used by stub resolver
+  networking.nameservers = [
+    "9.9.9.9#dns.quad9.net"
+    "149.112.112.112#dns.quad9.net"
+  ];
+
+  services.resolved = {
+    enable = true;
+    settings.Resolve = {
+      DNSOverTLS = "true";
+      DNSSEC = "false";
+    };
+  };
 }
