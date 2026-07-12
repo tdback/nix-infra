f130df0599c5d17b775d60520ed4aa4357199e58
tdback <tyler@tdback.net>
2026-07-12 14:46:54 +0000

    set domain

--- common/default.nix
+++ common/default.nix
@@ -4,6 +4,7 @@
     ./hardware.nix
     ./kernel.nix
     ./locale.nix
+    ./network.nix
     ./nix.nix
     ./security.nix
     ./users.nix
---
+++ common/network.nix
@@ -0,0 +1,3 @@
+{
+  networking.domain = "tdback.net";
+}
