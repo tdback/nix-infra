8b658613837d0ec317c60c312d13979b43fdb6d3
tdback <tyler@tdback.net>
2026-07-15 22:40:03 -0400

    add nixvim input

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05?shallow=true";
-  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable?shallow=true";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05?shallow=1";
+  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable?shallow=1";
 
-  inputs.flake-parts.url = "github:hercules-ci/flake-parts?shallow=true";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts?shallow=1";
   inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/release-26.05?shallow=true";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-26.05?shallow=1";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.agenix.url = "github:ryantm/agenix?shallow=true";
+  inputs.agenix.url = "github:ryantm/agenix?shallow=1";
   inputs.agenix.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.nixvim.url = "github:nix-community/nixvim?shallow=1";
 
   outputs =
     inputs@{ flake-parts, ... }:
