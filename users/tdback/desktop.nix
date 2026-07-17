5f6966cda6e9dedf724d3c007e668f42a3ba6199
tdback <tyler@tdback.net>
2026-07-16 22:19:14 -0400

    add direnv

--- users/tdback/desktop.nix
+++ users/tdback/desktop.nix
@@ -1,3 +1,5 @@
 {
-  imports = [ ];
+  imports = [
+    ./dots/direnv.nix
+  ];
 }
---
+++ users/tdback/dots/direnv.nix
@@ -0,0 +1,11 @@
+{
+  config,
+  ...
+}:
+{
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = config.programs.bash.enable;
+    nix-direnv.enable = true;
+  };
+}
