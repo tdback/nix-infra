f0b799741f2eb682d93e49e94c5b16d462e0e730
tdback <tyler@tdback.net>
2026-07-18 10:39:04 -0400

    tidy up inputs

--- flake.nix
+++ flake.nix
@@ -1,20 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05?shallow=1";
-  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable?shallow=1";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  inputs.flake-parts.url = "github:hercules-ci/flake-parts?shallow=1";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/release-26.05?shallow=1";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-26.05";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.agenix.url = "github:ryantm/agenix?shallow=1";
+  inputs.agenix.url = "github:ryantm/agenix";
   inputs.agenix.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.nixvim.url = "github:nix-community/nixvim?shallow=1";
+  inputs.nixvim.url = "github:nix-community/nixvim";
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    { flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } (
       { ... }:
       {
