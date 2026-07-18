1a127c3c8aea9b8cfe487179c9fab733b857dd7e
tdback <tyler@tdback.net>
2026-07-18 10:25:03 -0400

    set colorscheme on desktop systems

--- users/tdback/dots/nvim.nix
+++ users/tdback/dots/nvim.nix
@@ -1,50 +1,75 @@
 {
   inputs,
   config,
+  lib,
+  desktop,
   ...
 }:
 {
   imports = [ inputs.nixvim.homeModules.nixvim ];
 
-  programs.nixvim = {
-    enable = true;
-    defaultEditor = true;
+  programs.nixvim = lib.mkMerge [
+    {
+      enable = true;
+      defaultEditor = true;
 
-    globals = {
-      mapleader = " ";
-      maplocalleader = " ";
-    };
+      globals = {
+        mapleader = " ";
+        maplocalleader = " ";
+      };
 
-    opts = {
-      relativenumber = true;
-      shiftwidth = 2;
-      expandtab = true;
-      wrap = false;
-      hlsearch = false;
-      incsearch = true;
-      signcolumn = "yes";
-      backup = false;
-      swapfile = false;
-      undofile = true;
-      undodir = "${config.xdg.cacheHome}/nvim/undodir";
-    };
+      opts = {
+        relativenumber = true;
+        shiftwidth = 2;
+        expandtab = true;
+        wrap = false;
+        hlsearch = false;
+        incsearch = true;
+        signcolumn = "yes";
+        termguicolors = desktop;
+        backup = false;
+        swapfile = false;
+        undofile = true;
+        undodir = "${config.xdg.cacheHome}/nvim/undodir";
+      };
 
-    autoCmd = [
-      {
-        event = [ "BufWritePre" ];
-        command = "%s/\\s\\+$//e";
-      }
-      {
-        event = [ "TextYankPost" ];
-        callback.__raw = ''
-          function()
-            vim.highlight.on_yank({
-              higroup = "IncSearch",
-              timeout = 50,
-            })
-          end
-        '';
-      }
-    ];
-  };
+      autoCmd = [
+        {
+          event = [ "BufWritePre" ];
+          command = "%s/\\s\\+$//e";
+        }
+        {
+          event = [ "TextYankPost" ];
+          callback.__raw = ''
+            function()
+              vim.highlight.on_yank({
+                higroup = "IncSearch",
+                timeout = 50,
+              })
+            end
+          '';
+        }
+      ];
+    }
+
+    (lib.mkIf desktop {
+      plugins = {
+        treesitter = {
+          enable = true;
+          settings = {
+            highlight.enable = true;
+            indent.enable = true;
+          };
+        };
+      };
+
+      colorschemes.rose-pine = {
+        enable = true;
+        settings.styles = {
+          italic = false;
+          transparency = true;
+        };
+      };
+    })
+  ];
 }
