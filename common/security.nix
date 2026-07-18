b1596ce6fd72d296658f6516f8095d06946f1703
tdback <tyler@tdback.net>
2026-07-18 11:24:44 -0400

    disable systemd-boot editor

--- common/security.nix
+++ common/security.nix
@@ -6,4 +6,7 @@
   # require a password for interactive logins
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = (config.users.users.tdback.hashedPasswordFile != null);
+
+  # prevent privilege escalation via kernel parameters
+  boot.loader.systemd-boot.editor = false;
 }
