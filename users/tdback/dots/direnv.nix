bc03edcbd129621f3236bfc4477c0f76f77207eb
tdback <tyler@tdback.net>
2026-07-19 12:13:24 -0400

    disable warnings

--- users/tdback/dots/direnv.nix
+++ users/tdback/dots/direnv.nix
@@ -7,5 +7,9 @@
     enable = true;
     enableBashIntegration = config.programs.bash.enable;
     nix-direnv.enable = true;
+    config = {
+      strict_env = true;
+      warn_timeout = 0;
+    };
   };
 }
