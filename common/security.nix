6c7bcab5396f0b3e4d95bd46bd00447dc6888178
tdback <tyler@tdback.net>
2026-08-05 22:20:59 -0400

    tweak kernel runtime parameters

--- common/security.nix
+++ common/security.nix
@@ -26,4 +26,38 @@
 
   # Prevent privilege escalation via kernel parameters.
   boot.loader.systemd-boot.editor = false;
+
+  boot.kernel.sysctl = {
+    # Disable the magic SysRq key.
+    "kernel.sysrq" = 0;
+
+    # Reverse path filtering - prevents IP spoofing from DDoS attacks.
+    "net.ipv4.conf.all.rp_filter" = 1;
+    "net.ipv4.conf.default.rp_filter" = 1;
+
+    # Protect against SYN flood attacks.
+    "net.ipv4.tcp_syncookies" = 1;
+
+    # Protect against TIME-WAIT assassination hazards.
+    "net.ipv4.tcp_rfc1337" = 1;
+
+    # Disable source routing.
+    "net.ipv4.conf.all.accept_source_route" = 0;
+    "net.ipv6.conf.all.accept_source_route" = 0;
+
+    # Refuse ICMP redirects - mitigates MITM.
+    "net.ipv4.conf.all.accept_redirects" = 0;
+    "net.ipv4.conf.default.accept_redirects" = 0;
+    "net.ipv4.conf.all.secure_redirects" = 0;
+    "net.ipv4.conf.default.secure_redirects" = 0;
+    "net.ipv6.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.default.accept_redirects" = 0;
+
+    # Don't send ICMP redirects.
+    "net.ipv4.conf.all.send_redirects" = 0;
+    "net.ipv4.conf.default.send_redirects" = 0;
+
+    # Ignore ICMP broadcasts - mitigates SMURF.
+    "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
+  };
 }
